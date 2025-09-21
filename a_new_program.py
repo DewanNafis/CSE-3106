35f510d06e9195e64aa00facf5727dcd91646abf
dewan_nafis <dewannafisiqbal@gmail.com>
2025-09-21 15:19:34 +0600

    first commit to the branch

--- a_new_program.py
+++ a_new_program.py
@@ -1,7 +1,10 @@
+def sum(a, b):
+    return a + b
 def greet(name):
     return f"Hello, {name}!"
 def main():
     print(greet("GitHub"))
+    print(sum(5, 10))
 
 if __name__ == "__main__":
     main()
